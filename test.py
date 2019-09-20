46519984207177409b8a866507bc5b45623c7b74
Andre Machado <csantos.machado@gmail.com>
2019-09-20 16:35:35 -0300

    Update test.py

--- test.py
+++ test.py
@@ -66,6 +66,80 @@ class TestAcceptanceStripe(unittest.TestCase):
         res = re.search(pattern, self.dom_str)
         self.assertTrue(hasattr(res, 'group'), msg="You didn't define a cancel URL.")
 
+
+class AssessmentTestCases(unittest.TestCase):
+    def setUp(self):
+        with open("order.html", "r") as file_descriptor:
+            self.dom_str = file_descriptor.read()
+
+        CHROMEDRIVER_PATH = os.getenv('CHROMEDRIVERDIR')
+        WINDOW_SIZE = "1920,1080"
+
+        options = selenium.webdriver.ChromeOptions()
+        options.headless = True
+        options.add_argument("--window-size=%s" % WINDOW_SIZE)
+        options.add_argument("--disable-gpu")
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+
+        self.driver = webdriver.Chrome(
+            executable_path=CHROMEDRIVER_PATH, options=options
+        )
+
+    def _get_button_id(self):
+        pattern = re.compile(r"\('checkout-button-sku_'\);", re.I | re.M)
+        res = re.search(pattern, self.dom_str)
+        return res.group().split("'")[1]
+
+    def _get_url(self):
+        pattern = re.compile(
+            r"cancelUrl: \'(http|https)://(.*)/order.html\'", re.I | re.M
+        )
+        res = re.search(pattern, self.dom_str)
+        return ":".join(res.group().split(": ")[1:]).strip("'")
+
+    def test_assessment_successful_payment_on_the_checkout_page_redirects_to_order_html(
+        self
+    ):
+        self.driver.get(self._get_url())
+        wait = WebDriverWait(self.driver, 20)
+
+        elem = wait.until(EC.presence_of_element_located((By.ID, self._get_button_id())))
+        elem.click()
+
+        email_elem = wait.until(EC.presence_of_element_located((By.ID, "email")))
+
+        cardnum_elem = self.driver.find_element_by_id("cardNumber")
+        cardexp_elem = self.driver.find_element_by_id("cardExpiry")
+        cardcvc_elem = self.driver.find_element_by_id("cardCvc")
+        cardname_elem = self.driver.find_element_by_id("billingName")
+
+        try:
+            zip_elem = self.driver.find_element_by_id('billingPostalCode')
+        except NoSuchElementException:
+            zip_elem = None
+
+        email_elem.send_keys("assessment@test.com.br")
+        cardnum_elem.send_keys("4242424242424242")
+        cardexp_elem.send_keys("0439")
+        cardcvc_elem.send_keys("424")
+        cardname_elem.send_keys("Selenium Test WebDriver")
+
+        if zip_elem:
+            zip_elem.send_keys('12312')
+
+        confirm_elem = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "SubmitButton--complete")))
+        confirm_elem.click()
+
+        session_id_elem = wait.until(
+            EC.presence_of_element_located((By.ID, "sessionId"))
+        )
+
+        self.assertIn("order_success.html", self.driver.current_url)
+        self.assertTrue(session_id_elem.text)
+
+    def tearDown(self):
+        self.driver.close()
  
 if __name__ == '__main__':
     unittest.main()
